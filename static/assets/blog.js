5f32c03f23f467ec6f166994eecdaa0d820110f4
starry <115192496+sky22333@users.noreply.github.com>
2025-02-25 18:24:07 +0800

    更新 blog.js

--- static/assets/blog.js
+++ static/assets/blog.js
@@ -1,1 +1,1 @@
-defer src="https://api.adminlove.me/script.js" data-website-id="68b84108-618a-4b39-a4cc-6ab3bdd869c7">
+defer src="https://api.adminlove.me/script.js" data-website-id="68b84108-618a-4b39-a4cc-6ab3bdd869c7"
